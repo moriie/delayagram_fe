6ab1df53bf5fe37d35a42f093f9d4223d1e6b458
jwy5140 <jwy5140@gmail.com>
2020-07-16 01:55:57 -0400

    Added css to login form.

--- src/Login/login.js
+++ src/Login/login.js
@@ -4,15 +4,20 @@ import styled from 'styled-components'
 const Login = () => {
 
     const [identity, setIdentity] = useState('')
+    const [password, setPassword] = useState('')
 
     const handleOnSubmit = (e) => {
         e.preventDefault()
         console.log(identity)
+        console.log(password)
+
+        
     }
 
     return <LoginBlock onSubmit={(e)=>handleOnSubmit(e)}>
         <IdentityInput value={identity} onChange={e=>setIdentity(e.target.value)}></IdentityInput><br/>
-        <Submit type="submit">Submit</Submit>
+        <PasswordInput value={password} type='password' onChange={e=>setPassword(e.target.value)}></PasswordInput><br/>
+        <Submit type="submit">Login</Submit>
     </LoginBlock>
 }
 
@@ -21,15 +26,20 @@ export default Login
 //styles
 
 const LoginBlock = styled.form`
-
+    width: 20%;
+    height: 40vh;
+    margin: 30vh auto;
+    border: 1px solid black;
 `
 
 const IdentityInput = styled.input`
     border: 1px solid black;
+    border-radius: 3px;
 `
 
 const PasswordInput = styled.input`
     border: 1px solid black;
+    border-radius: 3px;
 `
 
 const Submit = styled.button`

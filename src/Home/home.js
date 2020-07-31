d322056dea44dc26927acfbbe2877e2ae4a7dce8
jwy5140 <jwy5140@gmail.com>
2020-07-31 19:50:44 -0400

    Added footer and links to underlying component. Styled footer.

--- src/Home/home.js
+++ src/Home/home.js
@@ -1,10 +1,23 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Home = () => {
 
     return <Splash>
-        <p>sample text</p>
+        <Footer>
+            <Link to="/about">ABOUT</Link>
+            <Link to="/help">HELP</Link>
+            <Link to="/press">PRESS</Link>
+            <Link to="/api">API</Link>
+            <Link to="/jobs">JOBS</Link>
+            <Link to="/privacy">PRIVACY</Link>
+            <Link to="/terms">TERMS</Link>
+            <Link to="/locations">LOCATIONS</Link>
+            <Link to="/topaccounts">TOP ACCOUNTS</Link>
+            <Link to="/hashtags">HASHTAGS</Link>
+            <Link to="/language">LANGUAGE</Link>
+        </Footer>
     </Splash>
 }
 
@@ -14,4 +27,14 @@ export default Home
 
 const Splash = styled.div`
     background-color: rgb(250, 250, 250);
+    height: 100vh;
+    width: 100vw;
+`
+
+const Footer = styled.div`
+    display: flex;
+    /* font-size: 12px; ---FIX LATER--- */
+    margin: 0 20vw;
+    flex-direction: row;
+    justify-content: space-between;
 `

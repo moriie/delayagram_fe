1443b3c3fa18a2d3d692bf5ffda464f2cf2ec24c
jwy5140 <jwy5140@gmail.com>
2020-07-30 23:52:57 -0400

    Started main page, added routes.

---
+++ src/Home/home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Home = () => {
+
+    return
+        <Splash>
+            
+        </Splash>
+}
+
+//styles
+
+const Splash = styled.div`
+`
--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import React from 'react'
 
+import Login from './Login/login'
+import Signup from './Signup/signup'
+
 export const Routes = () => {
 
     return 
         <Router>
             <Switch>
-                
+                <Route path="/login" component={Login}/>
+                <Route path="/signup" component={Signup}/>
             </Switch>
         </Router>
 }
